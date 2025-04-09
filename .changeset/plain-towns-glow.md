adc342bbe06c9e4a38b82b0f745c3c4d5a211ead
Danilo Tuler <tuler@pobox.com>
2025-04-09 14:43:13 -0400

    Export additional types

---
+++ .changeset/plain-towns-glow.md
@@ -0,0 +1,5 @@
+---
+"@cartesi/viem": patch
+---
+
+export additional types
--- packages/viem/src/index.ts
+++ packages/viem/src/index.ts
@@ -1,6 +1,10 @@
 export * from "./actions/index.js";
-export { createCartesiPublicClient } from "./clients/createCartesiPublicClient.js";
+export {
+    createCartesiPublicClient,
+    type CartesiPublicClient,
+} from "./clients/createCartesiPublicClient.js";
 export { publicActionsL1 } from "./decorators/publicL1.js";
 export { publicActionsL2 } from "./decorators/publicL2.js";
 export { walletActionsL1 } from "./decorators/walletL1.js";
+export * from "./types/actions.js";
 export * from "./utils/index.js";
