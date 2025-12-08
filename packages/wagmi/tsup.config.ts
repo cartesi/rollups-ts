d6dfa9ebf75842782b0b5d55350cd624e700ac44
Danilo Tuler <tuler@pobox.com>
2025-12-08 17:55:23 -0500

    use client banner

--- packages/wagmi/tsup.config.ts
+++ packages/wagmi/tsup.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
     clean: true,
     dts: true,
     entry: ["src/index.ts"],
+    esbuildOptions(options) {
+        options.banner = {
+            js: '"use client";',
+        };
+    },
     external: ["@tanstack/react-query", "react", "viem", "wagmi"],
     format: ["cjs", "esm"],
     minify: true,
