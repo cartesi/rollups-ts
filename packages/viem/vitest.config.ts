692dff68073a1c9381ce00aada7ca1e1011fd81e
Bruno Menezes <brunodmenezes@gmail.com>
2026-05-22 18:48:47 +0100

    chore(package/viem): Add vitest configuration, add coverage-v8 and bump vitest version.

---
+++ packages/viem/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        dir: "./__tests__",
+        exclude: ["node_modules/**"],
+        environment: "node",
+        globals: true,
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "lcov"],
+            exclude: ["./src/rollups.ts"],
+            include: ["**/src"],
+        },
+    },
+});
