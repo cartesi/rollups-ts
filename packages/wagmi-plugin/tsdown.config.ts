4f27d3471e7cfbf42a0d9de5093979f777aab81a
Danilo Tuler <tuler@pobox.com>
2026-07-09 21:16:39 -0400

    chore: @cartesi/wagmi-plugin

--- packages/wagmi-plugin/tsdown.config.ts
+++ packages/wagmi-plugin/tsdown.config.ts
@@ -3,12 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
     clean: true,
     dts: true,
-    entry: [
-        "src/index.ts",
-        "src/download.ts",
-        "src/wagmi/plugins/index.ts",
-        "src/wagmi/plugins/deployments.ts",
-    ],
+    entry: ["src/index.ts"],
     fixedExtension: false,
     format: ["cjs", "esm"],
     minify: true,
